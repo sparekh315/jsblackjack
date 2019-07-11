1d5c77df85b8b06cfe064f3d21e388512409ea98
Sania Parekh <sparekh@Sanias-MacBook.local>
2019-07-11 13:22:57 -0500

    startd app for blackjack game

---
+++ blackjack.js
@@ -0,0 +1,10 @@
+//Blackjack App
+//by Sania Parekh
+
+let card1 = "Ace of Spades",
+    card2 = "Ten of Hearts";
+
+console.log("Welcome to Blackjack!");
+console.log("You are Dealt: ");
+console.log(" " + card1);
+console.log(" " + card2);
